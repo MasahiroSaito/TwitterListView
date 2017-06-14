a02e69afc908e01c10cfe7d9a80bb58c2bc13cfd
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-06-14 13:48:33 +0900

    Create MyListStatusView

---
+++ app/src/main/java/com/masahirosaito/twitterlistview/view/MyListStatusView.kt
@@ -0,0 +1,38 @@
+package com.masahirosaito.twitterlistview.view
+
+import android.content.Context
+import android.support.constraint.ConstraintLayout
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.masahirosaito.twitterlistview.R
+import com.masahirosaito.twitterlistview.bindView
+import com.masahirosaito.twitterlistview.model.MyListStatus
+
+class MyListStatusView : ConstraintLayout {
+
+    constructor(context: Context,
+                attrs: AttributeSet,
+                defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    constructor(context: Context,
+                attrs: AttributeSet) : super(context, attrs)
+
+    constructor(context: Context) : super(context)
+
+    val userIconImageView: ImageView by bindView(R.id.user_icon)
+    val userNameTextView: TextView by bindView(R.id.user_name)
+    val textTextView: TextView by bindView(R.id.text)
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.view_myliststatus, this)
+    }
+
+    fun setMyListStatus(myListStatus: MyListStatus) {
+        userNameTextView.text = myListStatus.user.name
+        textTextView.text = myListStatus.text
+        Glide.with(context).load(myListStatus.user.profile_image_url).into(userIconImageView)
+    }
+}
