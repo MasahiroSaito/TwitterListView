614e8ec2ef96a57d43644921c0daf40df4171293
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-06-14 13:24:55 +0900

    Create Value object

---
+++ app/src/main/java/com/masahirosaito/twitterlistview/Value.kt
@@ -0,0 +1,8 @@
+package com.masahirosaito.twitterlistview
+
+object Value {
+    val consumerKey: String = ""
+    val consumerSecret: String = ""
+    val accessToken: String = ""
+    val accessTokenSecret: String = ""
+}
