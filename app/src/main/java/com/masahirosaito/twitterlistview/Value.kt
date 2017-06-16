81eea2254696514241a1a2aeed47f61f943ad9ca
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-06-16 09:36:22 +0900

    Gradle acceleration

--- app/src/main/java/com/masahirosaito/twitterlistview/Value.kt
+++ app/src/main/java/com/masahirosaito/twitterlistview/Value.kt
@@ -1,8 +1,8 @@
 package com.masahirosaito.twitterlistview
 
 object Value {
-    val consumerKey: String = ""
-    val consumerSecret: String = ""
-    val accessToken: String = ""
-    val accessTokenSecret: String = ""
+    val consumerKey: String = "Kah7AIz1NmD0QoviTPH4YHcGW"
+    val consumerSecret: String = "eWA4300X1BKFn3704q6ZgfdXJ7lnAj7VQepJQBWQHaNxbDOjtM"
+    val accessToken: String = "827576743783387136-WoaCwtCWqYlpIte7EvPHZmw6RGj09tw"
+    val accessTokenSecret: String = "PHMpRl1bWeyuwJWENUQbi0GBu2e8rTQHF6PvEEXJdLgcU"
 }
